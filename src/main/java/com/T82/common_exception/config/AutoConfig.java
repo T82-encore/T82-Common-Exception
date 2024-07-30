fa6169d8fd4c0c9ec106735b2e03279fafd24f43
parkhyunseo <dazzle1999@naver.com>
2024-07-30 11:36:09 +0900

    feat: test filter and autoConfig 설정

---
+++ src/main/java/com/T82/common_exception/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.T82.common_exception.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.T82.common_exception")
+public class AutoConfig {
+}
---
+++ src/main/java/com/T82/common_exception/filter/ErrorFilter.java
@@ -0,0 +1,24 @@
+package com.T82.common_exception.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class ErrorFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.error("**************test*************");
+        log.error("**************test*************");
+        log.error("**************test*************");
+        log.error("**************test*************");
+    }
+}
