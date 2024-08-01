c4a551cbcfb6a0cf249d0a5e5146ba824e044320
parkhyunseo <dazzle1999@naver.com>
2024-08-01 21:50:27 +0900

    feature: 예외시 Error logging 추가

--- src/main/java/com/T82/common_exception/aspect/CustomExceptionAspect.java
+++ src/main/java/com/T82/common_exception/aspect/CustomExceptionAspect.java
@@ -2,6 +2,7 @@ package com.T82.common_exception.aspect;
 
 import com.T82.common_exception.annotation.HandleCustomException;
 import com.T82.common_exception.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class CustomExceptionAspect {
 
     @Around("@annotation(handleException)")
@@ -16,7 +18,9 @@ public class CustomExceptionAspect {
         try {
             return joinPoint.proceed();
         } catch (Exception e) {
-            throw new CustomException(handleException.value());
+            CustomException customException = new CustomException(handleException.value());
+            log.error("ERROR : {} ({})", customException.getErrorCode().getMessage(), customException.getErrorCode().getCode(), e);
+            throw customException;
         }
     }
 }
--- src/main/java/com/T82/common_exception/exception/ErrorCode.java
+++ src/main/java/com/T82/common_exception/exception/ErrorCode.java
@@ -7,7 +7,16 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
+    /**
+     * ******************************* Global Error CodeList ***************************************
+     * HTTP Status Code
+     * 400 : Bad Request
+     * 401 : Unauthorized
+     * 403 : Forbidden
+     * 404 : Not Found
+     * 500 : Internal Server Error
+     * *********************************************************************************************
+     */
     COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON-001", "쿠폰을 찾을 수 없습니다."),
     COUPON_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON-002", "존재하지 않는 카테고리 입니다."),
     MIN_PURCHASE_NOT_ACCEPT(HttpStatus.BAD_REQUEST, "COUPON-003", "쿠폰을 사용할 수 있는 최소 금액을 만족하지 않습니다."),
