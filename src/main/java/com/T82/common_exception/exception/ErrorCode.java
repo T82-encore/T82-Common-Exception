eb89ea6ee0dc942e266cafe821ccaf9f5f21af1d
parkhyunseo <dazzle1999@naver.com>
2024-08-01 23:32:40 +0900

    fix: update

--- src/main/java/com/T82/common_exception/exception/ErrorCode.java
+++ src/main/java/com/T82/common_exception/exception/ErrorCode.java
@@ -22,7 +22,6 @@ public enum ErrorCode {
     MIN_PURCHASE_NOT_ACCEPT(HttpStatus.BAD_REQUEST, "COUPON-003", "쿠폰을 사용할 수 있는 최소 금액을 만족하지 않습니다."),
     COUPON_EXPIRED(HttpStatus.BAD_REQUEST, "COUPON-004", "만료된 쿠폰입니다.");
 
-
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
