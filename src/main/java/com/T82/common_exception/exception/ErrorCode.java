69220b3b8891e108a3d3c24431403e049b1265d5
parkhyunseo <dazzle1999@naver.com>
2024-08-02 12:26:06 +0900

    feature: ErrorCode 추가

--- src/main/java/com/T82/common_exception/exception/ErrorCode.java
+++ src/main/java/com/T82/common_exception/exception/ErrorCode.java
@@ -19,8 +19,9 @@ public enum ErrorCode {
      */
     COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON-001", "쿠폰을 찾을 수 없습니다."),
     COUPON_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "COUPON-002", "존재하지 않는 카테고리 입니다."),
-    MIN_PURCHASE_NOT_ACCEPT(HttpStatus.BAD_REQUEST, "COUPON-003", "쿠폰을 사용할 수 있는 최소 금액을 만족하지 않습니다."),
-    COUPON_EXPIRED(HttpStatus.BAD_REQUEST, "COUPON-004", "만료된 쿠폰입니다.");
+    MIN_PURCHASE_NOT_ACCEPTED(HttpStatus.BAD_REQUEST, "COUPON-003", "쿠폰을 사용할 수 있는 최소 금액을 만족하지 않습니다."),
+    COUPON_EXPIRED(HttpStatus.BAD_REQUEST, "COUPON-004", "만료된 쿠폰입니다."),
+    COUPON_VALIDATE_FAILED(HttpStatus.BAD_REQUEST, "COUPON-005", "쿠폰 검증이 실패하였습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
