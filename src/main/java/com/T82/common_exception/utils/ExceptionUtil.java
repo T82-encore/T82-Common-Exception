d8691fc5b55fc46024c1035b4c4cb43ccdc02fba
parkhyunseo <dazzle1999@naver.com>
2024-08-07 09:16:26 +0900

    fix: ExceptionUtil 수정

--- src/main/java/com/T82/common_exception/utils/ExceptionUtil.java
+++ src/main/java/com/T82/common_exception/utils/ExceptionUtil.java
@@ -1,9 +1,9 @@
-package com.T82.common_exception.util;
+package com.T82.common_exception.utils;
 
 import com.T82.common_exception.exception.CustomException;
 import com.T82.common_exception.exception.ErrorCode;
 
-public class ExceptionUtils {
+public class ExceptionUtil {
     public static CustomException createCustomException(ErrorCode errorCode) {
         return new CustomException(errorCode);
     }
