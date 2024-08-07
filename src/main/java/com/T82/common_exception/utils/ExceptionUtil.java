b9eaa2927b5a06fc83f60645c96ce0e84d7b8e2e
parkhyunseo <dazzle1999@naver.com>
2024-08-07 09:25:42 +0900

    fix: update Exception Util

--- src/main/java/com/T82/common_exception/utils/ExceptionUtil.java
+++ src/main/java/com/T82/common_exception/utils/ExceptionUtil.java
@@ -2,7 +2,9 @@ package com.T82.common_exception.utils;
 
 import com.T82.common_exception.exception.CustomException;
 import com.T82.common_exception.exception.ErrorCode;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ExceptionUtil {
     public static CustomException createCustomException(ErrorCode errorCode) {
         return new CustomException(errorCode);
